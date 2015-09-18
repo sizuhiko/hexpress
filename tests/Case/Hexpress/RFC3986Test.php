64644ea8c440b4644fb5c55829a55357c521b4ae
sizuhiko <k-kishida@esm.co.jp>
2015-09-18 12:39:04 +0900

    fix misunderstand rules

--- tests/Case/Hexpress/RFC3986Test.php
+++ tests/Case/Hexpress/RFC3986Test.php
@@ -3,6 +3,10 @@ namespace Test\Hexpress;
 
 use Hexpress\Hexpress;
 
+/**
+ * RFC3986 pattern match test case.
+ * @see https://www.ietf.org/rfc/rfc3986.txt
+ */
 class RFC3986Test extends \PHPUnit_Framework_TestCase
 {
     private $hexpress;
@@ -14,11 +18,14 @@ class RFC3986Test extends \PHPUnit_Framework_TestCase
     public function testMatchURI()
     {
         $this->uri();
-//        var_dump($this->hexpress->toRegExp());
+        var_dump($this->hexpress->toRegExp());
         preg_match($this->hexpress->toRegExp(), "http://user:password@example.com:8080/path/to/file?date=1342460570#fragment", $matches);
-//        var_dump($matches);
+        var_dump($matches);
     }
 
+    /**
+     * URI = scheme ":" hier-part [ "?" query ] [ "#" fragment ]
+     */
     private function uri()
     {
         $this->hexpress
@@ -30,59 +37,94 @@ class RFC3986Test extends \PHPUnit_Framework_TestCase
              ->end()
              ;
     }
+    /**
+     * ALPHA *( ALPHA / DIGIT / "+" / "-" / "." )
+     */
     private function scheme()
     {
         return (new Hexpress())
             ->find(function($hex){
-                $hex->matching(function($hex){ $hex->letter(); })
-                    ->matching(function($hex){ $hex->letter()->number()->with("+-."); })
-                    ->many(NULL, 0);
+                $hex->many(function($hex){
+                    $hex->matching(function($hex){ $hex->letter(); })
+                        ->matching(function($hex){ $hex->letter()->number()->with("+-."); });
+                }, 0);
             });
     }
+    /**
+     * hier-part = "//" authority path-abempty
+     *             / path-absolute
+     *             / path-rootless
+     *             / path-empty
+     */
     private function hierPart()
     {
         return (new Hexpress())
-            ->has("//")
-            ->find($this->authority())
             ->find(function($hex) {
-                $hex->either([$this->pathAbempty(), $this->pathAbsolute(), $this->pathRootless(), $this->pathEmpty()]);
+                $hex->either([
+                    (new Hexpress())->has("//")->find($this->authority())->find($this->pathAbempty()),
+                    $this->pathAbsolute(),
+                    $this->pathRootless(),
+                    $this->pathEmpty()
+                ]);
               });
     }
+    /**
+     * authority = [ userinfo "@" ] host [ ":" port ]
+     */
     private function authority()
     {
         return (new Hexpress())->maybe($this->userinfo())->find($this->host())->maybe($this->port());
     }
+    /**
+     * userinfo = *( unreserved / pct-encoded / sub-delims / ":" )
+     */
     private function userinfo()
     {
         return (new Hexpress())
             ->find(function($hex){
-                $hex->either([
-                    $this->unreserved(),
-                    $this->pctEncoded(),
-                    $this->subDelims(),
-                    ":"
-                  ]);
+                $hex->many(function($hex){
+                    $hex->either([
+                            $this->unreserved(),
+                            $this->pctEncoded(),
+                            $this->subDelims(),
+                            ":"
+                          ]);
+                }, 0);
               })
-            ->many(NULL, 0)
             ->has("@");
     }
+    /**
+     * unreserved = ALPHA / DIGIT / "-" / "." / "_" / "~"
+     */
     private function unreserved()
     {
         return (new Hexpress())->matching(function($hex) { $hex->letter()->number()->with("-._~"); });
     }
+    /**
+     * pct-encoded = "%" HEXDIG HEXDIG
+     */
     private function pctEncoded()
     {
         return (new Hexpress())->has("%")->limit(function($hex) { $hex->matching(function($hex) { $hex->number()->upper(); }); }, 2);
     }
+    /**
+     *  sub-delims = "!" / "$" / "&" / "'" / "(" / ")" / "*" / "+" / "," / ";" / "="
+     */
     private function subDelims()
     {
         return (new Hexpress())->matching(function($hex){ $hex->has("!$&'()*+,;="); });
     }
+    /**
+     * host = IP-literal / IPv4address / reg-name
+     */
     private function host()
     {
         // TODO not support IP-literal
         return (new Hexpress())->either([$this->IPv4address(), $this->regName()]);
     }
+    /**
+     * IPv4address = dec-octet "." dec-octet "." dec-octet "." dec-octet
+     */
     private function IPv4address()
     {
         return (new Hexpress())
@@ -92,17 +134,27 @@ class RFC3986Test extends \PHPUnit_Framework_TestCase
             ->has($this->decOctet())
             ;
     }
+    /**
+     * dec-octet  = DIGIT                 ; 0-9
+     *            / %x31-39 DIGIT         ; 10-99
+     *            / "1" 2DIGIT            ; 100-199
+     *            / "2" %x30-34 DIGIT     ; 200-249
+     *            / "25" %x30-35          ; 250-255
+     */
     private function decOctet()
     {
         return (new Hexpress())
             ->either([
                 (new Hexpress())->number(),
                 (new Hexpress())->number([1,9])->number(),
-                (new Hexpress())->has("1")->number([0,9])->number([0,9]),
-                (new Hexpress())->has("2")->number([0,4])->number([0,9]),
+                (new Hexpress())->has("1")->number()->number(),
+                (new Hexpress())->has("2")->number([0,4])->number(),
                 (new Hexpress())->has("25")->number([0,5])
             ]);
     }
+    /**
+     * reg-name = *( unreserved / pct-encoded / sub-delims )
+     */
     private function regName()
     {
         return (new Hexpress())
@@ -114,14 +166,26 @@ class RFC3986Test extends \PHPUnit_Framework_TestCase
                 ]);
             });
     }
+    /**
+     * port = *DIGIT
+     */
     private function port()
     {
         return (new Hexpress())->has(":")->find(function($hex) { $hex->digits(); });
     }
+    /**
+     * path-abempty = *( "/" segment )
+     * segment      = *pchar
+     */
     private function pathAbempty()
     {
         return (new Hexpress())->many(function($hex){ $hex->has("/")->many($this->pchar(), 0); }, 0);
     }
+    /**
+     * path-absolute = "/" [ segment-nz *( "/" segment ) ]
+     * segment-nz    = 1*pchar
+     * segment       = *pchar
+     */
     private function pathAbsolute()
     {
         return (new Hexpress())
@@ -132,8 +196,13 @@ class RFC3986Test extends \PHPUnit_Framework_TestCase
                         $hex->has("/");
                         $hex->many($this->pchar(), 0);
                     });
-                });     
+                });
     }
+    /**
+     * path-rootless = segment-nz *( "/" segment )
+     * segment-nz    = 1*pchar
+     * segment       = *pchar
+     */
     private function pathRootless()
     {
         return (new Hexpress())
@@ -143,10 +212,17 @@ class RFC3986Test extends \PHPUnit_Framework_TestCase
                 $hex->many($this->pchar(), 0);
               });
     }
+    /**
+     * path-empty    = 0<pchar>
+     * segment       = *pchar
+     */
     private function pathEmpty()
     {
         return (new Hexpress())->without($this->pchar());
     }
+    /**
+     * query = *( pchar / "/" / "?" )
+     */
     private function query()
     {
         return (new Hexpress())
@@ -157,6 +233,9 @@ class RFC3986Test extends \PHPUnit_Framework_TestCase
                 }, 0);
             });
     }
+    /**
+     * fragment = *( pchar / "/" / "?" )
+     */
     private function fragment()
     {
         return (new Hexpress())
@@ -167,6 +246,9 @@ class RFC3986Test extends \PHPUnit_Framework_TestCase
                 }, 0);
             });
     }
+    /**
+     * pchar = unreserved / pct-encoded / sub-delims / ":" / "@"
+     */
     private function pchar()
     {
         return (new Hexpress())
