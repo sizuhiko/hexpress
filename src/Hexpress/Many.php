aee16c49417eccd4b300f1906aff9a34211b18e8
sizuhiko <k-kishida@esm.co.jp>
2016-05-29 16:49:42 +0900

    fix phpcs

--- src/Hexpress/Many.php
+++ src/Hexpress/Many.php
@@ -6,7 +6,7 @@ trait Many
 {
     public function many($value = null, $minimum = 1)
     {
-        if($minimum > 1) {
+        if ($minimum > 1) {
             return $this->limit($value, $minimum);
         }
         return $this->addValues(ManyValue::class, $value, $minimum);
